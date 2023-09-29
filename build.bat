9d8d359b0600c3995190560572c9ee14a1011a1c
felocsss <vlad.shipilov.felocs@gmail.com>
2023-09-29 18:06:44 +0300

    Initial commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+"D:\Fortnite\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project=F:\UE_projects\DevOpsProject\DevOpsProject.uproject ^
+platform=Win64 -clientconfig=Development ^
+-archivedirectory=F:\UE_projects\DevOpsProject\Builds ^
+-cook -stage -archive -pak -package -allmaps
